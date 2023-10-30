4ffdbe1f3af82858f757ed0f5a43b5ebd3dff9ae
ajw00026 <ajw00026@mix.wvu.edu>
2023-10-30 15:52:45 -0400

    navbar and card

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function NavBar() {
     );
 }
 
-export NavBar;
+export { NavBar };
 
 function Card() {
     return (
@@ -68,7 +68,7 @@ function Card() {
     );
 }
 
-export default Card;
+export { Card };
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
