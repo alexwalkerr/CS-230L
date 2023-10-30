8ac825159d08824508fae70ad3dd98bda93fd1bb
ajw00026 <ajw00026@mix.wvu.edu>
2023-10-30 15:41:26 -0400

    navbar and card

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
+        <NavBar />
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800365400</p>
       <p>Hi I am Alex</p>
-	  <NavBar />
-
         <Card />
     </div>
   );
