dc1c5a59a8f721d3f954bc8f634384257324f70c
ajw00026 <ajw00026@mix.wvu.edu>
2023-10-30 16:54:49 -0400

    navbar and cards

--- src/index.js
+++ src/index.js
@@ -42,15 +42,34 @@ function NavBar() {
 
 function Card() {
     return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
-            <div className="card-body">
-                <h5 className="card-title">Card 1</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="d-flex justify-content-center">
+            <div className="card" style={{ width: "18rem", margin: "0 10px" }}>
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card 1</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+
+            <div className="card" style={{ width: "18rem", margin: "0 10px" }}>
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card 2</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+
+            <div className="card" style={{ width: "18rem", margin: "0 10px" }}>
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card 3</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
         </div>
     );
 }
+
 
 export { NavBar, Card };
 
