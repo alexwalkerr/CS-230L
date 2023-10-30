3027e98b19088de9940fd7a51e8da0697518cb8b
ajw00026 <ajw00026@mix.wvu.edu>
2023-10-30 16:50:52 -0400

    new navbar

--- src/index.js
+++ src/index.js
@@ -22,26 +22,12 @@ function NavBar() {
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav">
                     <li className="nav-item active">
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
@@ -53,14 +39,14 @@ function NavBar() {
     );
 }
 
+
 function Card() {
     return (
         <div className="card" style={{ width: "18rem" }}>
             <img className="card-img-top" src="..." alt="Card image cap" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Card 1</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     );
